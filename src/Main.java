6c5966d3d76dfba6e75f77511aa0bad6de9c699a
VincentZimbiziAngel <56736887+VincentZim@users.noreply.github.com>
2023-07-15 02:08:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -166,6 +166,32 @@ public class Exercise5 {
         return count; // returns 0 if string starts or ends with space " ".
     }
  }
+// Write a Java method to compute the sum of digits in an integer.
+
+import java.util.Scanner;
+public class Exercise6 {
+
+  public static void main(String[] args)
+    {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Input an integer: ");
+      int digits = in.nextInt();
+	  System.out.println("The sum is " + sumDigits(digits));
+    }
+
+ public static int sumDigits(long n) {
+		int result = 0;
+		
+		while(n > 0) {
+			result += n % 10;
+			n /= 10;
+		}
+		
+		return result;
+	}
+	
+ }
+
 
 
 
