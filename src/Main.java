095014fb424c62689afc87f4d67026ed1fa665b2
VincentZimbiziAngel <56736887+VincentZim@users.noreply.github.com>
2023-07-15 02:02:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -81,3 +81,32 @@ public class main {
         return Math.min(Math.min(x, y), z);
     }
 }
+// Ex 3. Write a Java method to display the middle character of a string.
+
+import java.util.Scanner;
+public class Exercise3 {
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String str = in.nextLine();
+        System.out.print("The middle character in the string: " + middle(str)+"\n");
+    }
+ public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return str.substring(position, position + length);
+    }
+}
+
