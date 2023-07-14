b7d4efc7451fe8c25fbbd85dd1f4df95da0de429
VincentZimbiziAngel <56736887+VincentZim@users.noreply.github.com>
2023-07-15 01:13:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text);
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println("Box 2");
-    }
-}
-
-// methods 
+// intro to methods 
 
 public class Main {
 
@@ -69,4 +58,26 @@ public class Main {
         System.out.println(181.2);
     }
 
+}
+//  Ex 1. Write a Java method to find the smallest number among three numbers.
+
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double x = in.nextDouble();
+        System.out.print("Input the Second number: ");
+        double y = in.nextDouble();
+        System.out.print("Input the third number: ");
+        double z = in.nextDouble();
+        System.out.print("The smallest value is " + smallest(x, y, z)+"\n" );
+    }
+
+    public static double smallest(double x, double y, double z)
+    {
+        return Math.min(Math.min(x, y), z);
+    }
 }
