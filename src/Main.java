a424d290d61cc6ec2aa3a35d281626634a90ad62
VincentZimbiziAngel <56736887+VincentZim@users.noreply.github.com>
2023-07-16 23:11:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -238,7 +238,21 @@ public class Exercise8 {
  public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
 		return investmentAmount * Math.pow(1 + monthlyInterestRate, years * 12);
 	}
+} 
+// Write a Java method to print characters between two characters (i.e. A to P).
+public class Exercise9 {
+   public static void main(String[] args) {
+        print_Chars('(', 'z', 20);
+    }
+ public static void print_Chars(char char1, char char2, int n) {
+        for (int ctr = 1; char1 <= char2; ctr++, char1++) {
+            System.out.print(char1 + " ");
+            if (ctr % n == 0) System.out.println("");
+        }
+		System.out.print("\n");
+    }
 }
+
 
 
 
