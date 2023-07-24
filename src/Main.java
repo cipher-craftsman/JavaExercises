756b76ff99e834b149ef0c84ef5e15e5cc9f67d4
Vincent Zimbizi Angel <56736887+VincentZim@users.noreply.github.com>
2023-07-24 23:13:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -381,8 +381,31 @@ public class Solution {
 }
 
 
+//Write a Java program that accepts four integers from the user and prints equal if all four are equal, and not equal otherwise.
 
-
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		 System.out.print("Input first number: ");
+         int num1 = in.nextInt();
+         System.out.print("Input second number: ");
+         int num2 = in.nextInt();
+		 System.out.print("Input third number: ");
+         int num3 = in.nextInt();
+         System.out.print("Input fourth number: ");
+         int num4 = in.nextInt();		
+		
+		if (num1 == num2 && num2 == num3 && num3 == num4) 
+		  {
+			System.out.println("Numbers are equal.");
+                               }
+		else
+			{
+			System.out.println("Numbers are not equal!");
+		}
+	}
+}
 	
 
 
