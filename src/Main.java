f29fc133b0fafea8028928d98d3779d38fb796de
Vincent Zimbizi Angel <56736887+VincentZim@users.noreply.github.com>
2023-07-25 23:15:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -422,8 +422,11 @@ public class Solution {
 }
 
 // Write a Java program to print the contents of a two-dimensional Boolean array where t represents true and f represents false.
-import java.util.Scanner;
-public class Solution {
+// Sample array:
+// array = {{true, false, true},
+// {false, true, false}};ort java.util.Scanner;
+
+import public class Solution {
 	public static void main(String[] args) {
 		boolean[][] array = {{true, false, true},
 							 {false, true, false}};
@@ -445,6 +448,49 @@ public class Solution {
     }
 }
 
+// Write a Java program to print an array after changing the rows and columns of a two-dimensional array. 
+//Original Array:
+//10 20 30
+//40 50 60
+//After changing the rows and columns of the said array:10 40
+//20 50
+//30 60
+
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		int[][] twodm = {
+				{10, 20, 30},
+				{40, 50, 60}
+		};
+		System.out.print("Original Array:\n");
+		print_array(twodm);
+		System.out.print("After changing the rows and columns of the said array:");
+		transpose(twodm);
+		}
+	
+	private static void transpose(int[][] twodm) {
+		
+		int[][] newtwodm = new int[twodm[0].length][twodm.length];
+		
+		for (int i = 0; i < twodm.length; i++) {
+			for (int j = 0; j < twodm[0].length; j++) {
+				newtwodm[j][i] = twodm[i][j];
+			}
+		}
+		
+		print_array(newtwodm);
+	}
+	private static void print_array(int[][] twodm) {
+		for (int i = 0; i < twodm.length; i++) {
+			for (int j = 0; j < twodm[0].length; j++) {
+				System.out.print(twodm[i][j] + " ");
+			}
+			System.out.println();
+		}
+	
+	}
+}
 
 	
 
