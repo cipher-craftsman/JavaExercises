bfae8c1a328ffec3358b3fe97bd7676bf5315f56
Vincent Zimbizi Angel <56736887+VincentZim@users.noreply.github.com>
2023-07-27 00:44:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -491,6 +491,41 @@ public class Solution {
 	
 	}
 }
+//Write a Java program to create a two-dimensional array (m x m) A[][] such that A[i][j] is false if I and j are prime otherwise A[i][j] becomes true.
+import java.util.Scanner;
+public class Solution {
+	public static int prime_cell(int a, int b) {
+		if (a == 0 || b == 0) {
+			return 1;
+		}
+		if (a < b) {
+			int t = a;
+			a = b;
+			b = t;
+		}
+		if (a % b == 0) 
+		{
+			return b;
+		} 
+		else 
+		{
+			return prime_cell(b, a % b);
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		boolean[][] A = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				A[i][j] = prime_cell(i, j) == 1;
+				System.out.print(A[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
 
 	
 
